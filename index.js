77d3df08b4e7f47a0d20cdd01b1535332e8792e0
Ivan Santiago Gutierrez Estrada <55468122+TheTerribleS@users.noreply.github.com>
2022-08-13 18:42:09 -0500

    json parse, try 1

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
-let fullPortfolio = JSON.parse();
+function GetPortfolio()
+{
+    var request = new XMLHttpRequest();
+    request.open("GET", "resources/sources.json", false);
+    request.send(null);
+    var parse = JSON.parse(request.responseText);
+    alert(parse);
+    return parse;
+}
+
+
+
+
+const fullPortfolio = GetPortfolio();
+
+
+
+console.log(fullPortfolio);
 
 
 
 
 
-
-//DP19KI-DP19KI-DP19KI-DP19KI-DP19KI-DP19KI-DP19KI-DP19KI-DP19KI-DP19KI-DP19KI-DP19KI-DP19KI-DP19KI-DP19KI
+//DP19KI-DP19KI-DP19KI-DP19KI-DP19KI-DP19KI-DP19KI-DP19KI-DP19KI-DP19KI-DP19KI-DP19KI-DP19KI-DP19KI-DP19KI-DP19KI-DP19KI
